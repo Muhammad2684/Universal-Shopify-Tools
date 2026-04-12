98114d96ea9b3980f1d5aadf1678fa73169afe83
Muhammad2684 <picbbpk@gmail.com>
2026-04-12 21:08:16 +0500

    Added Product Notes

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def credentials_ok():
 # VERSION / AUTO-UPDATE
 # ════════════════════════════════════════════════════════════════════════════
 
-APP_VERSION = "1.1.6"
+APP_VERSION = "1.1.7"
 VERSION_URL = "https://raw.githubusercontent.com/Muhammad2684/Universal-Shopify-Tools/main/version.json"
 
 _update_state = {"status": "idle", "percent": 0, "error": ""}
